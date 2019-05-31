a4302879b2847ba345db8959c74c837897126422
xxRevenge__ <32039975+Bowlingtoolkit@users.noreply.github.com>
2019-05-31 18:01:54 +0300

    Add files via upload

---
+++ install all packages in one click.bat
@@ -0,0 +1,69 @@
+@echo off
+echo This Will Take a while ! 
+
+npm i discord.js
+npm i async
+npm i erlpack
+npm i node-opus
+npm i request
+npm i hh-mm-ss
+npm i simple-youtube-api
+npm i youtube-info
+npm i uws
+npm i youtube-search
+npm i express
+npm i ytdl-core
+npm i http
+npm i array-sort
+npm i ascii-data-table
+npm i canvas
+npm i canvas-constructor
+npm i chess.js
+npm i common-tags
+npm i discord.js-musicbot-addon
+npm i eris
+npm i express
+npm i ffmpeg binaries
+npm i figlet
+npm i fianlhandler
+npm i fnpm
+npm i forever
+npm i fs
+npm i fs-nextra
+npm i get-youtube-id
+npm i gif-search
+npm i giphy-api
+npm i goo.gl
+npm i google-it
+npm i hastebin.js
+npm i i
+npm i install
+npm i jimp
+npm i isgd
+npm i moment
+npm i math-expression-evaluator
+npm i moment-duration-format
+npm i ms 
+npm i myanimelists
+npm i node-emoji
+npm i node-gyp
+npm i node uci
+npm i npm
+npm i opusscript
+npm i pretty-ms
+npm i python
+npm i queue
+npm i quick.db
+npm i random-number
+npm i rebuild
+npm i router
+npm i simple-youtube-api
+npm i snekfetch
+npm i sqlite
+npm i sqlite3
+npm i table
+npm i youtube-info
+npm i youtube-search
+npm i zalgolize
+
+pause
